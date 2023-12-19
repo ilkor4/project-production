77b711345467b24cac1c110b4a279c182b03db91
ilkor4 <kornelyuck.ilya@yandex.ru>
2023-12-19 09:49:08 +0300

    feat: Добавить ErrorBoundary

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,10 +1,12 @@
 import { useTranslation } from 'react-i18next';
+import { BugButton } from 'app/providers/ErrorBoundary/ index';
 
 const MainPage = () => {
     const { t } = useTranslation('main');
 
     return (
         <div>
+            <BugButton />
             {t('Главная страница')}
         </div>
     );
